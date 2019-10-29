1e27fc982fbc8ec96e553e0601b5cf2491af84f2
Carter Richmond <Cman5687@gmail.com>
2019-10-29 15:12:35 -0400

    Initial Commit

---
+++ app/src/main/java/edu/wit/mobile_health/pillow_companion/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package edu.wit.mobile_health.pillow_companion.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
