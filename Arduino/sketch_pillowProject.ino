3a92e3318d7502789506f8728807086c24af3c9d
Ben Clermont <benclermontt@gmail.com>
2019-11-22 14:20:54 -0500

    Update sketch_pillowProject.ino

--- Arduino/sketch_pillowProject.ino
+++ Arduino/sketch_pillowProject.ino
@@ -1,103 +1,52 @@
-#define LIGHTWEIGHT 1
-
-#include <SPI.h>
-#include "Adafruit_BLE_UART.h"
-#include <aREST.h>
-
-#define ADAFRUITBLE_REQ 10
-#define ADAFRUITBLE_RDY 2
-#define ADAFRUITBLE_RST 9
-
-aREST rest = aREST();
-
-Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
-
-const int MPU_addr=0x68;
-
-int accelX, accelY, accelZ;
-int temp = 0;
-int light = 0;
-boolean vibrate = false;
-
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  BTLEserial.begin();
+  while(Serial.available() == 0) {}
+  int num = Serial.parseInt();
 
-  rest.variable("temp", &temp);
-  rest.variable("light", &light);
-
-  rest.set_id("001");
-  rest.set_name("Pillow");
-  pinMode(5, OUTPUT);
-
-  BTLEserial.setDeviceName("Pillow");
+  factorial(num);
+  Serial.println();
+  fibNum(num);
 }
-
-aci_evt_opcode_t laststatus = ACI_EVT_DISCONNECTED;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  BTLEserial.pollACI();
 
-  aci_evt_opcode_t status = BTLEserial.getState();
-
-  light = analogRead(A5);
-  temp = analogRead(A4);
-
-  if(vibrate) {
-    vibration();
-  }
-
-  /*
-  Serial.print("Temp = ");
-  Serial.println(temp);
-  Serial.print("Light = ");
-  Serial.println(light);
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
-  */
-  
-  if(status != laststatus){
-    //print it out
-    if(status == ACI_EVT_DEVICE_STARTED){
-      Serial.println(F("* Advertising started"));
-    }
-    
-    if(status == ACI_EVT_CONNECTED){
-      Serial.println(F("* Connected!"));
-    }
-    
-    if(status == ACI_EVT_DISCONNECTED){
-      Serial.println(F("* Disconnected or advertising timed out"));
-    }
-    
-    // Set the last status
-    laststatus = status;
-  }
-  
-  // Handle REST calls
-  if(status == ACI_EVT_CONNECTED){
-    if(BTLEserial.available()) {
-      Serial.print("* ");
-      Serial.print(BTLEserial.available());
-      Serial.println(F(" bytes available from BTLE"));
-    }
-
-    while(BTLEserial.available()) {
-      char c = BTLEserial.read();
-      Serial.print(c);
-    }
-    
-    rest.handle(BTLEserial);
-  }
-  delay(300);
 }
 
+void factorial(int x) {
+  output(0,1);
+  
+  long long val = 1;
+  for(long long i = 1; i <= x; i++) {
+    val *= i;
+    output(i, val);
+  }
+}
 
-void vibration() {
-  digitalWrite(5, HIGH);
-  delay(2000);
-  digitalWrite(5, LOW);
+void fibNum(int x) {
+  long long a = 0, b = 1, c;
+  if(x == 0)
+    return a;
+   for(long i=2; i <= x; i++) {
+    c = a+b;
+    a = b;
+    b=c;
+   }
+
+   outputFib(x, c);
+}
+
+void output(long long a, long long p){
+  Serial.print("The factorial of ");
+  Serial.print(a);
+  Serial.print(" = ");
+  Serial.println(p);
+}
+
+void outputFib(long a, long p){
+  Serial.print("The Fibonacci Number ");
+  Serial.print(a);
+  Serial.print(" = ");
+  Serial.println(p);
 }
